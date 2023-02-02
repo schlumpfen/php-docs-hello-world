61a214ec60426f9ce7614b1ccbbb45cb9f7d5a82
Stefan Roschewitz <stefan@roschewitz.de>
2023-02-02 20:40:24 +0100

    Update index.php
    Ganz anders

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<html> <!-- Start HTML -->
+<head> <!-- Start HTML-Kopf -->
+<!-- CSS-Formatierung für die Tabelle -->
+<style type="text/css">
+table {
+background-color: #E2E9F6;
+border-collapse: collapse;
+border: none;
+}
+thead {
+background-color: #4D79C7;
+color: #FFFFFF;
+}
+td, th {
+text-align: left;
+padding: 0.5em 1em;
+}
+</style> <!-- Ende CSS-Style -->
+</head> <!-- Ende HTML-Kopf -->
+<body> <!-- Start HTML-Body -->
+<?php //Start PHP
 
-echo "Hello Azure!";
-
-?>
+echo "Hello Azure!";  
+?> <!-- Ende PHP-->
+</body> <!-- Ende HTML-Body-->
+</html> <!-- Ende HTML-->
