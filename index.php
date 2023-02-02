7d33be63629019bbfbe024dd12c10583db904cfb
Stefan Roschewitz <stefan@roschewitz.de>
2023-02-02 21:55:49 +0100

    Update index.php
    Tabelle mit array

--- index.php
+++ index.php
@@ -18,13 +18,37 @@ padding: 0.5em 1em;
 </style> <!-- Ende CSS-Style -->
 </head> <!-- Ende HTML-Kopf -->
 <body> <!-- Start HTML-Body -->
-  <table>
-    <tr><td>a</td></tr>
-    <tr><td>b</td></tr>
-  </table>
-  <?php //Start PHP
-
-echo "Hello Azure!";  
+<?php //Start PHP
+//Parameter holen und ausgeben
+$bnr = $_GET["bnr"];
+echo "<h1>iBrot-Bestellung</h1>";
+echo "<h2>Bestellnummer: $bnr </h2>";
+/* Daten in einem assoziativen Array definieren*/
+$daten = array (
+array("Position"=>1,"ArtikelID"=>"Brötchen","Menge"=>5),
+array("Position"=>2,"ArtikelID"=>"Croissant","Menge"=>3),
+array("Position"=>3,"ArtikelID"=>"Brot","Menge"=>1)
+);
+/* Überschriften im Tabellen-Head ausgeben */
+echo "<table>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>Position</th>";
+echo "<th>Artikel-ID</th>";
+echo "<th>Menge</th>";
+echo "</tr>";
+echo "</thead>";
+/* Datensätze im Tabellen-Body ausgeben */
+echo "<tbody>";
+foreach ($daten as $row) {
+echo "<tr>";
+echo "<td>".$row['Position']."</td>";
+echo "<td>".$row['ArtikelID']."</td>";
+echo "<td>".$row['Menge']."</td>";
+echo "</tr>";
+}
+echo "</table>";
+echo "<tbody>";
 ?> <!-- Ende PHP-->
 </body> <!-- Ende HTML-Body-->
 </html> <!-- Ende HTML-->
